2265d2e5712386a35be6450f074e813d4d327e61
Henrry Valenzuela <henrryvalen@gmail.com>
2022-01-23 00:37:13 -0500

    Update index.vue

--- pages/metamask/index.vue
+++ pages/metamask/index.vue
@@ -1,17 +1,42 @@
 <template>
   <v-container
     ><h1>Metamask {{ bnbwalletAdress }}</h1>
-    <v-divider class="my-5"></v-divider>
-    <v-btn color="primary" small class="my-3" @click="conectarMetamask()"
+    <v-divider class="my-1"></v-divider>
+    <v-btn color="green" small class="my-3" @click="conectarMetamask()"
       >Conectar metamask</v-btn
     >
+    <v-divider class="my-1"></v-divider>
+    <v-btn color="yellow" small class="my-3" @click="cambiarTestnetBSCS()"
+      >Red Testnet BSCS</v-btn
+    >
+    <v-btn color="yellow" small class="my-3" @click="cambiarProdnetBSCS()"
+      >Red Prod BSCS</v-btn
+    >
     <v-card>
-      <v-card-title>
-        <v-icon class="mr-3"> mdi-account-key </v-icon>
-        Botsito
-      </v-card-title>
       <v-card-text>
         <div>
+          <v-text-field
+            dense
+            outlined
+            label="Red binance"
+            v-model="buscarToken.redBinance"
+          >
+          </v-text-field>
+          <v-text-field
+            dense
+            outlined
+            label="Factory"
+            v-model="buscarToken.factoryContrato"
+          >
+          </v-text-field>
+          <v-text-field
+            dense
+            outlined
+            label="Router"
+            v-model="buscarToken.reouterContrato"
+          >
+          </v-text-field>
+
           <v-text-field
             dense
             outlined
@@ -26,10 +51,17 @@
             v-model="buscarToken.tokenContrato"
           >
           </v-text-field>
+          <v-text-field
+            dense
+            outlined
+            label="Cantidad Bnb"
+            v-model="buscarToken.cantidadBnb"
+          >
+          </v-text-field>
+          <v-text-field dense outlined label="Gas Price" v-model="buscarToken.gas">
+          </v-text-field>
           <div class="d-flex justify-space-between">
-            <v-btn color="primary" small @click="obtenerParBNB()">
-              Buscar
-            </v-btn>
+            <v-btn color="primary" small @click="obtenerParBNB()"> Buscar </v-btn>
           </div>
 
           <v-divider class="my-5"></v-divider>
@@ -48,12 +80,29 @@ export default {
       buscarToken: {
         wbnbContrato: "",
         tokenContrato: "",
+        redBinance: "",
+        factoryContrato: "",
+        reouterContrato: "",
+        cantidadBnb: "",
+        gas: "",
       },
       contratoLiq: "",
       precioToken: "",
     };
   },
   methods: {
+    cambiarTestnetBSCS() {
+      this.buscarToken.wbnbContrato = "0xae13d989dac2f0debff460ac112a837c89baa7cd";
+      this.buscarToken.redBinance = "https://data-seed-prebsc-1-s1.binance.org:8545";
+      this.buscarToken.factoryContrato = "0x6725F303b657a9451d8BA641348b6761A6CC7a17";
+      this.buscarToken.reouterContrato = "0xD99D1c33F9fC3444f8101754aBC46c52416550D1";
+    },
+    cambiarProdnetBSCS() {
+      this.buscarToken.wbnbContrato = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
+      this.buscarToken.redBinance = "https://bsc-dataseed.binance.org/";
+      this.buscarToken.factoryContrato = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73";
+      this.buscarToken.reouterContrato = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+    },
     conectarMetamask() {
       if (typeof window.ethereum !== "undefined") {
         conectar()
@@ -94,14 +143,13 @@ export default {
       const web3 = require("web3");
       let wbnb = this.buscarToken.wbnbContrato;
       let token = this.buscarToken.tokenContrato;
+      let cantidadBnb = this.buscarToken.cantidadBnb;
 
-      const provider = new ethers.providers.JsonRpcProvider(
-        "https://bsc-dataseed.binance.org/"
-      );
+      const provider = new ethers.providers.JsonRpcProvider(this.buscarToken.redBinance);
 
       const contract = {
-        factory: "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73", // PancakeSwap V2 factory
-        router: "0x10ED43C718714eb63d5aA57B78B54704E256024E", // PancakeSwap V2 router
+        factory: this.buscarToken.factoryContrato, //"0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73", // PancakeSwap V2 factory
+        router: this.buscarToken.reouterContrato, // PancakeSwap V2 router
       };
 
       const router = new ethers.Contract(
@@ -125,37 +173,50 @@ export default {
       } */
 
       if (true) {
-        getPrice(wbnb, token).then((price) => {
+        getPrice(cantidadBnb, wbnb, token).then((price) => {
           this.precioToken = price;
+
+          if (this.precioToken != "") {
+            //var originalAmountToBuyWith = "0.007" + Math.random().toString().slice(2, 7);
+            //console.log(originalAmountToBuyWith)
+
+            //var bnbAmount = web3.utils.toWei(cantidadBnb.toString().slice(2, 7), "ether");
+            //console.log(parseFloat(cantidadBnb) * 1000000000)
+            //var res = 
+            //var contador = 0
+            //while(contador==0){
+              buyOnlyone(
+              this.bnbwalletAdress,
+              parseFloat(cantidadBnb) * 1000000000,
+              wbnb,
+              token,
+              contract.router,
+              this.buscarToken.redBinance,
+              this.buscarToken.gas
+            ).then( (txHash) => {
+                console.log(txHash)
+             //   contador = contador+1;
+            }).catch((e) => {
+              console.log(e);
+              obtenerParBNB();
+            });
+            //console.log(contador);
+           // }
+
+
+            ///*  */console.log("res -->" + res);
+          }
         });
 
         getPair(wbnb, token).then((l) => {
           this.contratoLiq = "https://bscscan.com/address/" + l;
-
-          console.log(this.contratoLiq)
-        /* if (this.contratoLiq != "") {
-          var originalAmountToBuyWith =
-            "0.007" + Math.random().toString().slice(2, 7);
-          var bnbAmount = web3.utils.toWei(originalAmountToBuyWith, "ether");
-
-          var res = buyOnlyone(
-            this.bnbwalletAdress,
-            bnbAmount,
-            wbnb,
-            token,
-            contract.router
-          );
-          console.log("res -->" + res);
-        } */
-
         });
-        
       } else {
         console.log("ERROR");
       }
 
-      async function getPrice(bnbContrato, tokenContrato) {
-        let bnbToSell = web3.utils.toWei("1", "ether");
+      async function getPrice(cantidadBNB, bnbContrato, tokenContrato) {
+        let bnbToSell = web3.utils.toWei(cantidadBNB, "ether");
         const amounts = await router.getAmountsOut(bnbToSell, [
           bnbContrato,
           tokenContrato,
@@ -165,16 +226,13 @@ export default {
       }
 
       async function getPair(bnbContrato, tokenContrato) {
-        const contratoLiquidez = await factory.getPair(
-          bnbContrato,
-          tokenContrato
-        );
+        const contratoLiquidez = await factory.getPair(bnbContrato, tokenContrato);
         return contratoLiquidez.toString();
       }
 
       //COMPRAR!
 
-      var BSC_FORK = Common.forCustomChain(
+      /*   var BSC_FORK = Common.forCustomChain(
         "mainnet",
         {
           name: "Binance Smart Chain Mainnet",
@@ -183,24 +241,27 @@ export default {
           url: "https://bsc-dataseed.binance.org/",
         },
         "istanbul"
-      );
+      ); */
 
       async function buyOnlyone(
         targetAccount,
         amount,
         wbnToken,
         tokenAddress,
-        routeAddress
+        routeAddress,
+        redBinance,
+        gas
       ) {
-          var Web3 = require('web3')
-          var web3 = new Web3(new Web3.providers.HttpProvider('https://bsc-dataseed.binance.org/'))
+        var Web3 = require("web3");
+        var web3 = new Web3(new Web3.providers.HttpProvider(redBinance));
         var amountToBuyWith = web3.utils.toHex(amount);
 
         /* var tokenAddress = "0x8301f2213c0eed49a7e28ae4c3e91722919b8b47"; // ONLYONE contract address
          var WBNBAddress = "0xae13d989dac2f0debff460ac112a837c89baa7cd"; // WBNB token address
         */
 
-        var amountOutMin = "100" + Math.random().toString().slice(2, 6);
+        //var amountOutMin = "100" + Math.random().toString().slice(2, 6);
+        var amountOutMin = "0";
         /* var pancakeSwapRouterAddress =
           "0xD99D1c33F9fC3444f8101754aBC46c52416550D1"; */
 
@@ -217,9 +278,7 @@ export default {
               { internalType: "uint256", name: "deadline", type: "uint256" },
             ],
             name: "swapExactETHForTokens",
-            outputs: [
-              { internalType: "uint256[]", name: "amounts", type: "uint256[]" },
-            ],
+            outputs: [{ internalType: "uint256[]", name: "amounts", type: "uint256[]" }],
             stateMutability: "payable",
             type: "function",
           },
@@ -227,6 +286,7 @@ export default {
         var contract = new web3.eth.Contract(routerAbi, routeAddress, {
           from: targetAccount,
         });
+        //console.log((Math.round(Date.now() / 1000) + 60 * 20))
         var data = contract.methods.swapExactETHForTokens(
           web3.utils.toHex(amountOutMin),
           [wbnToken, tokenAddress],
@@ -237,13 +297,13 @@ export default {
         var count = await web3.eth.getTransactionCount(targetAccount);
         let gasPrice = await web3.eth.getGasPrice();
         const gasLimit = 500000;
-
+        //console.log(amount)
         var rawTransaction = {
           from: targetAccount,
-          gasPrice: web3.utils.toHex(web3.utils.toWei("5", "gwei")),
+          gasPrice: web3.utils.toHex(web3.utils.toWei(gas, "gwei")),
           gasLimit: 0x13551,
           to: routeAddress,
-          value: web3.utils.toHex(web3.utils.toWei("10000000", "gwei")),
+          value: web3.utils.toHex(web3.utils.toWei(amount.toString(), "gwei")),
           data: data.encodeABI(),
           nonce: web3.utils.toHex(count),
         };
